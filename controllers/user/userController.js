aa065e1e912acac6840f1e383df381de7a620236
Arshad Chappangan <arshadchappangan349@gmail.com>
2025-03-08 11:46:50 +0530

    user home page and error page

---
+++ controllers/user/userController.js
@@ -0,0 +1,21 @@
+const loadHome = async (req,res) => {
+    try {
+        return res.render("home");
+    } catch (error) {
+        console.log("Home page not found");
+        res.status(500).send("Server Error")
+    }
+}
+
+const pageNotFound = async (req,res) => {
+    try {
+        res.render('page-404')
+    } catch (error) {
+        res.redirect('/pageNotFound')
+    }
+}
+
+module.exports = {
+    loadHome,
+    pageNotFound
+}
