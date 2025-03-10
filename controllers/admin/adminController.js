04a260f33cf527c8d994946177320d9b89b0201e
Arshad Chappangan <arshadchappangan349@gmail.com>
2025-03-11 00:37:43 +0530

    admin logout setup completed

--- controllers/admin/adminController.js
+++ controllers/admin/adminController.js
@@ -3,37 +3,37 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 
-const loadLogin = (req,res) => {
-    if(req.session.admin){
+const loadLogin = (req, res) => {
+    if (req.session.admin) {
         return res.redirect('/admin')
     }
-    res.render('adminLogin',{message:null})
+    res.render('adminLogin', { message: null })
 }
 
-const login = async (req,res) => {
+const login = async (req, res) => {
     try {
-        const {email,password} = req.body;
-        const admin = await User.findOne({email});
-        if(admin){
-            const passwordMatch = bcrypt.compare(password,admin.password);
-            if(passwordMatch){
+        const { email, password } = req.body;
+        const admin = await User.findOne({ email });
+        if (admin) {
+            const passwordMatch = bcrypt.compare(password, admin.password);
+            if (passwordMatch) {
                 req.session.admin = true;
                 return res.redirect('/admin')
-            }else{
-                return res.render('adminLogin',{message:"Invalid password"})
+            } else {
+                return res.render('adminLogin', { message: "Invalid password" })
             }
-        }else{
-            return res.render('adminLogin',{message:"Admin not found"})
+        } else {
+            return res.render('adminLogin', { message: "Admin not found" })
         }
     } catch (error) {
-        console.error("Admin login error : ",error);
+        console.error("Admin login error : ", error);
         return res.redirect('/pageError')
     }
 }
 
-const loadDashboard = async (req,res) => {
+const loadDashboard = async (req, res) => {
     try {
-        if(!req.session.admin){
+        if (!req.session.admin) {
             return res.redirect('/admin/login')
         }
         res.render('dashboard')
@@ -42,13 +42,29 @@ const loadDashboard = async (req,res) => {
     }
 }
 
-const pageError = async (req,res) => {
+const pageError = async (req, res) => {
     res.render("adminError")
+}
+
+const logout = async (req, res) => {
+    try {
+        req.session.destroy(error => {
+            if (error) {
+                console.log("Error in Destroying admin session : ", error);
+                return res.redirect('/pageError')
+            }
+            res.redirect("/admin/login")
+        })
+    } catch (error) {
+        console.log("Unexpected error during logout : ",error)
+        res.redirect('/pageError')
+    }
 }
 
 module.exports = {
     loadLogin,
     login,
     loadDashboard,
-    pageError
+    pageError,
+    logout
 }
